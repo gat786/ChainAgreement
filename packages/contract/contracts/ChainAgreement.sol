abc014a3cdb0cd56276fa9a2b86bf53fc3aa1536
ganesh tiwari <gat786@outlook.com>
2021-11-26 22:24:08 +0530

    created contract package and added chainagreement

---
+++ packages/contract/contracts/ChainAgreement.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract ChainAgreement {
+    struct Agreement {
+        address initiator;
+        address acceptor;
+        string title;
+        string content_id;
+    }
+    mapping(string => Agreement) agreements;
+
+    function createAgreement(
+        address _initiator,
+        address _acceptor,
+        string memory _title,
+        string memory _content_id
+    ) public returns (Agreement memory) {
+        Agreement memory agreement = Agreement({
+            initiator: _initiator,
+            acceptor: _acceptor,
+            title: _title,
+            content_id: _content_id
+        });
+        agreements[_content_id] = agreement;
+        return agreement;
+    }
+
+    function getAgreement(string memory _content_id)
+        public
+        view
+        returns (Agreement memory)
+    {
+        return agreements[_content_id];
+    }
+}
