77489d94fa37091caf998d39ef75c477ee1f57d9
ganesh tiwari <gat786@outlook.com>
2021-11-28 20:18:48 +0530

    created view agreement page

--- packages/frontend/src/pages/ViewPage.jsx
+++ packages/frontend/src/pages/ViewPage.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function ViewPage() {
   return (
-    <div>
-      View Page
+    <div className="flex flex-col items-center">
+      <div className="w-5/6 flex flex-col gap-4 mbs-8">
+        <h1 className="text-xl font-bold mlb-4">
+          View Agreement Page
+        </h1>
+
+        <label htmlFor="form-id">Enter Agreement ID to view</label>
+        <input type="text" className="p-2" name="" id="form-id" />
+        <button className="w-max px-4 py-2 bg-black font-semibold">View Agreement</button>
+      </div>
     </div>
   )
 }
