cb6ea646b45d86a29f9798d694e2dc5165fca2e1
Ganesh Tiwari <Ganesht049@gmail.com>
2021-12-05 21:07:08 +0530

    changed project to a typescript project

--- packages/api/contract/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router()
-
-router.get("/",(req,res) => {
-    res.send("You have reached contracts endpoint");
-})
-
-export default router;
