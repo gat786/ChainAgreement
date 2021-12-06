59fbddd27fca5ccba64336aa01f6b37d0ea0c139
Ganesh Tiwari <Ganesht049@gmail.com>
2021-12-06 22:15:16 +0530

    added tailwind plugins to vite app

--- packages/frontend-vite/src/pages/Homepage.jsx
+++ packages/frontend-vite/src/pages/Homepage.jsx
@@ -1,34 +1,26 @@
 import { useEffect, useState } from "react";
-import logo from '../logo.svg';
-import '../App.css';
+import logo from "../logo.svg";
+import "../App.css";
 
 import { Link } from "react-router-dom";
 
 function Homepage() {
-  const [user, setUser] = useState(null);
-
-  const Login = () => {
-    // Moralis.authenticate().then((user) => {
-    //   console.log(`${JSON.stringify(user)} has logged in`);
-    // })
-  }
-
   return (
     <div>
-      {user !== null ?
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center h-screen w-screen">
-          <Link to="create" className="text-2xl hover-scale w-max m-auto cursor-pointer">Create</Link>
-          <Link to="view" className="text-2xl hover-scale w-max m-auto cursor-pointer">View</Link>
-        </div>
-        : <div className="flex flex-col gap-4 items-center justify-center mlb-8">
-          <h1 className="text-2xl">
-            To be able to use the app please login
-          </h1>
-          <button onClick={Login} className="bg-black text-xl font-semibold px-4 py-2">
-            Login
-          </button>
-        </div>
-      }
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center h-screen w-screen">
+        <Link
+          to="create"
+          className="text-2xl hover-scale w-max m-auto cursor-pointer"
+        >
+          Create
+        </Link>
+        <Link
+          to="view"
+          className="text-2xl hover-scale w-max m-auto cursor-pointer"
+        >
+          View
+        </Link>
+      </div>
     </div>
   );
 }
--- packages/frontend-vite/tailwind.config.js
+++ packages/frontend-vite/tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      display: ["hover", "focus", "group-hover"],
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-logical"), require("@tailwindcss/typography")],
+};
